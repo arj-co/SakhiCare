641fdd13d6fad2f91108b9718fe38b469fd8aa11
arj-co <arjco@proton.me>
2026-09-11 11:21:54 +0530

    release: SakhiCare v5.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sakhicare.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 45
-        versionName = "4.5.0"
+        versionCode = 50
+        versionName = "5.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
